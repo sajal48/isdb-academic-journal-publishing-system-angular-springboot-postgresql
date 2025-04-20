22bd96d01462db3c60e1a3c3e857589b96f1638b
Sharier Himu <himusharier@gmail.com>
2025-04-20 07:54:31 +0600

    frontend homepage added

--- ajps-frontend/src/app/app.routes.ts
+++ ajps-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: 'home', pathMatch: 'full', component: HomepageComponent},
+
+    {path: '**', component: ErrorpageComponent}
+];
