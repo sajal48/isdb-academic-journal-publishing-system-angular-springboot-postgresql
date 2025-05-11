9bc60a140fd1850c1dc9a9a1a6fc6cb06b345160
Sharier Himu <himusharier@gmail.com>
2025-05-12 01:14:49 +0600

    header structure changed, view refactored, added few user pages

--- ajps-frontend/src/app/site-settings/view/userpanel-view/userpanel-view.component.ts
+++ ajps-frontend/src/app/site-settings/view/userpanel-view/userpanel-view.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../../components/footer/footer.component';
 import { UserSidebarComponent } from '../../../user/components/user-sidebar/user-sidebar.component';
-import { HeaderComponent } from "../../../components/header/header.component";
+import { UserHeaderComponent } from "../../../user/components/user-header/user-header.component";
 
 @Component({
   selector: 'app-userpanel-view',
-  imports: [RouterOutlet, FooterComponent, UserSidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, UserSidebarComponent, UserHeaderComponent],
   templateUrl: './userpanel-view.component.html',
   styleUrl: './userpanel-view.component.css'
 })
