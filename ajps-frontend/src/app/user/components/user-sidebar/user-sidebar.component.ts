3ffce698154af004306b6926df99a7c0122394ae
Sharier Himu <himusharier@gmail.com>
2025-05-12 13:11:45 +0600

    profile pages design updated

--- ajps-frontend/src/app/user/components/user-sidebar/user-sidebar.component.ts
+++ ajps-frontend/src/app/user/components/user-sidebar/user-sidebar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './user-sidebar.component.html',
   styleUrl: './user-sidebar.component.css'
 })
