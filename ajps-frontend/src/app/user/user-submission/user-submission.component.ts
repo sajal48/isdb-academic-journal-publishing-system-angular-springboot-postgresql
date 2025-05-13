055f0d364b3788fb5d5091bbcae93f0220c896e3
Sharier Himu <himusharier@gmail.com>
2025-05-13 11:38:23 +0600

    basic user panel design complete

--- ajps-frontend/src/app/user/user-submission/user-submission.component.ts
+++ ajps-frontend/src/app/user/user-submission/user-submission.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-submission',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-submission.component.html',
   styleUrl: './user-submission.component.css'
 })
